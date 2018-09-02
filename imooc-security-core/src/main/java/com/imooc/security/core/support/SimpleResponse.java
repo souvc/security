54ffabea985a212a725478b8aa3f65121826aa18
liuhongfeng <daliu_it@163.com>
2018-09-02 23:10:21 +0800

    修改响应信息类，让返回信息标准化

--- imooc-security-core/src/main/java/com/imooc/security/core/support/SimpleResponse.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/support/SimpleResponse.java
@@ -1,28 +1,55 @@
 /**
- * 
+ *
  */
 package com.imooc.security.core.support;
 
 /**
  * 简单响应的封装类
- * 
- * @author zhailiang
  *
+ * @author zhailiang
  */
 public class SimpleResponse {
-	
-	public SimpleResponse(Object content){
-		this.content = content;
-	}
-	
-	private Object content;
 
-	public Object getContent() {
-		return content;
-	}
+    private int code=200;
+    private Object data;
+    private String message="";
 
-	public void setContent(Object content) {
-		this.content = content;
-	}
-	
+    public SimpleResponse(String message) {
+        this.message = message;
+    }
+
+    public SimpleResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public SimpleResponse(int code, Object data, String message) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
